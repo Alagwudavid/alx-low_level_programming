db44d9770e54625d78d0a02017943f27e5519e4e
David Alagwu <alagwudavid2019@gmail.com>
2023-05-18 15:23:50 -0700

    puts instead of printf in c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/*
+ * main - use puts to print
+ * Return: "Programming is like building a multilingual puzzle and 0
+ */
+
+int main(){
+	puts("\"Programming is like building a multilingual puzzle\n");
+	return 0;
+}
