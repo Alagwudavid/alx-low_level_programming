70b33c791c5e5a0ef330c16fdeaccb88b39b7c83
David Alagwu <alagwudavid2019@gmail.com>
2023-05-18 15:27:51 -0700

    puts instead of printf in c - betty

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,10 +2,10 @@
 
 /*
  * main - use puts to print
- * Return: "Programming is like building a multilingual puzzle and 0
+ * Return: 0
  */
 
-int main(){
+int main(void) {
 	puts("\"Programming is like building a multilingual puzzle\n");
 	return 0;
 }
