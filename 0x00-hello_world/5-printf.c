077ba0be9cf590e5f4d0c17a1b81b4443e44597a
David Alagwu <alagwudavid2019@gmail.com>
2023-05-19 02:26:32 -0700

    c programming files

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,12 +1,15 @@
-#include<stdio.h>
+#include <stdio.h>
+
 /**
-* main - run printf
+* main - runs printf function
+*
 * Description - prints "the main function"
-* Return: 0
+*
+* Return: 0 success
 */
 
 int main(void)
 {
- printf("with proper grammar, but the outcome is a piece of art,");
- Return (0);
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
 }
