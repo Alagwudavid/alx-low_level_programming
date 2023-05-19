256819c29ac2d73cc9f3acfd7fee5cdca1c8b366
David Alagwu <alagwudavid2019@gmail.com>
2023-05-19 12:31:26 -0700

    display sentence without the use of printf and puts

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+* main - prints main fuction
+*
+* Description - prints "the function in main then returns 1"
+*
+* Return: 1 success
+*/
+
+int main(void)
+{
+	echo "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
+	return (1);
+}
