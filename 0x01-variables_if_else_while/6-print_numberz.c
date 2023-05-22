2b33e30c1cc07bad96e527c58054c2b8c9088c85
David Alagwu <alagwudavid2019@gmail.com>
2023-05-22 15:04:55 -0700

    print numberz

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -11,14 +11,12 @@
 
 int main(void)
 {
-    int number;
+	int number;
+	for (number = 0; number < 10; number++)
+	{
+	putchar('0' + number);
+	}
+	putchar('\n');
 
-    for (number = 0; number < 10; number++)
-    {
-        putchar('0' + number);
-    }
-
-    putchar('\n');
-
-    return 0;
+	return (0);
 }
