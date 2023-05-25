fe5f25c2820b1546272e6a5d92d299a876000858
Alagwudavid <alagwudavid2019@gmail.com>
2023-05-25 12:41:02 -0700

    3 files

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+* postitive_or_negative - print function
+* Description - tests function that prints if intege
+* 		 is positive or negative
+* Return: 0
+*/
+
+void postitive_or_negative(int i)
+{
+        i = 98;
+        positive_or_negative(i);
+
+        return (0);
+}
