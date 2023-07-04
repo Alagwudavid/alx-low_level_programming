935e88ede31f90879975a24dadc7506e9bd6f483
Alagwudavid <alagwudavid2019@gmail.com>
2023-07-03 19:07:11 -0700

    add node

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -10,43 +10,32 @@
  * Return: The address of the new list or NULL if it failed
  */
 
-list_t *add_node(list_t **head, const char *str){
-	list_t *temp;
-
-	if (head != NULL && str != NULL)
-	{
-		temp = malloc(sizeof(list_t));
-		if (temp == NULL)
-			return (NULL);
-
-		temp->str = strdup(str);
-		temp->len = _strlen(str);
-		temp->next = *head;
-
-		*head = temp;
-
-		return (temp);
-	}
-
-	return (0);
-}
-
-/**
- * _strlen - Returns the length of a string
- * @s: String to count
- *
- * Return: String length
- */
-
-int _strlen(const char *s)
+list_t *add_node(list_t **head, const char *str)
 {
-	int c = 0;
+	char *dup;
+	int len;
+	list_t *new;
 
-	while (*s)
+	new = malloc(sizeof(list_t));
+	if (new == NULL)
+		return (NULL);
+
+	dup = strdup(str);
+	if (dup == NULL)
 	{
-		s++;
-		c++;
+		free(new);
+		return (NULL);
 	}
 
-	return (c);
+	for (len = 0; str[len];)
+		len++;
+
+	new->str = dup;
+	new->len = len;
+	new->next = *head;
+
+	*head = new;
+
+	return (new);
 }
+
