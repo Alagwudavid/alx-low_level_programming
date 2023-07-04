061e89b075fcc6b5fc44a66bfe78ee3e9955a094
Alagwudavid <alagwudavid2019@gmail.com>
2023-07-03 18:19:10 -0700

    print string

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -3,12 +3,13 @@
 #include "lists.h"
 
 /**
-  * add_node - Adds a new node at the beginning of a list
-  * @head: The original linked list
-  * @str: The string to add to the node
-  *
-  * Return: The address of the new list or NULL if it failed
-  */
+ * add_node - Adds a new node at the beginning of a list
+ * @head: The original linked list
+ * @str: The string to add to the node
+ *
+ * Return: The address of the new list or NULL if it failed
+ */
+
 list_t *add_node(list_t **head, const char *str){
 	list_t *temp;
 
@@ -31,11 +32,12 @@ list_t *add_node(list_t **head, const char *str){
 }
 
 /**
-  * _strlen - Returns the length of a string
-  * @s: String to count
-  *
-  * Return: String length
-  */
+ * _strlen - Returns the length of a string
+ * @s: String to count
+ *
+ * Return: String length
+ */
+
 int _strlen(const char *s)
 {
 	int c = 0;
